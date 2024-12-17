84ebd68a450f94df694556f9a1dcbf3f91d29484
AenuHub <jesuskane92@gmail.com>
2024-12-17 15:48:40 +0300

    update main.py for constants

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 1 / 2
-SHORT_BREAK_MIN = 1 / 3
-LONG_BREAK_MIN = 1
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
 REPS = 0
 timer = None
 
